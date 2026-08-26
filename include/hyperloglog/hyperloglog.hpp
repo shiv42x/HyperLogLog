7a476075bb171125d7fde5366d267591c0f794bd
shiv42x <shivam2.patel@ryerson.ca>
2026-08-25 22:07:08 -0400

    rudimentary add() and estimate(), write basic test

--- include/hyperloglog/hyperloglog.hpp
+++ include/hyperloglog/hyperloglog.hpp
@@ -1,15 +1,20 @@
 #pragma once
-#include <vector>
+#include <atomic>
+#include <bit>
+#include <concepts>
 #include <cstdint>
+#include <cmath> 
+#include <gtest/gtest_prod.h>
+#include <vector>
+#include "MurmurHash3.h"
 
 namespace hll {
-
 template <typename T>
 class HyperLogLog {
     public:
         HyperLogLog() = delete;
         explicit HyperLogLog(uint8_t b):   
-            b_(b), m_(static_cast<size_t>(1) << b), registers_(m_, 0 ) {
+            b_(b), m_(static_cast<size_t>(1) << b), registers_(m_, 0) {
                 double alpha;
                 switch (m_) {
                     case 16:
@@ -28,20 +33,60 @@ class HyperLogLog {
                 alphamm_ = alpha * m_ * m_;
         }
 
-        void add() {
+        void add(const T& item) {
+            uint64_t hash = safe_hash(item, 42);
+            size_t idx = hash >> (64 - b_);
+            
+            uint8_t max_meaningful_zeros = 64 - b_;
+            uint8_t lz_count = std::min(
+                static_cast<uint8_t>(std::countl_zero(hash << b_)),
+                max_meaningful_zeros
+            );
+            
+            // +1 since we're storing position (1-indexed) of the first 1, not leading zeros count
+            registers_[idx] = std::max(registers_[idx], static_cast<uint8_t>(lz_count + 1));
         }
+        
         uint64_t estimate()  const {
+            // compute harmonic mean
+            double estimate;
+            double sum = 0.0;
+
+            size_t num_zeros = 0;
+
+            for (auto reg : registers_) {
+                if (reg == 0) ++num_zeros;
+                sum += 1.0 / (1 << reg);
+            }
+
+            if (num_zeros == m_) return 0;
+
+            estimate = alphamm_ / sum;
+            return static_cast<uint64_t>(std::floor(estimate));
         }
-        void merge(const HyperLogLog& other) {
-        }
+
         void clear() {
         }
 
     private:
-        uint8_t                 b_;
-        size_t                  m_;
-        double                  alphamm_;
-        std::vector<uint8_t>    registers_;
-}
+        uint8_t                             b_;         // no. of bits to index registers
+        size_t                              m_;         // no. of registers
+        double                              alphamm_;   // cache alpha * m * m (for estimate formula)
+        std::vector<uint8_t>                registers_; 
+       
+        uint64_t    safe_hash(const T& val, uint32_t seed) requires (std::integral<T> && !std::same_as<T, bool>) {
+            return hash_bytes(&val, sizeof(T), seed);
+        }
+
+        uint64_t    safe_hash(const std::string& str, uint32_t seed) requires (std::same_as<T, std::string>) {
+            return hash_bytes(str.data(), str.size(), seed);
+        }
+        
+        uint64_t    hash_bytes(const void* data, size_t len, uint32_t seed) {
+            std::uint64_t out[2];
+            MurmurHash3_x64_128(data, len, seed, out);
+            return out[0];
+        }
+};
 
 };
--- tests/test_hyperloglog.cpp
+++ tests/test_hyperloglog.cpp
@@ -1,2 +1,21 @@
 #include <gtest/gtest.h>
 #include "hyperloglog/hyperloglog.hpp"
+
+TEST(HLLTest, BasicTest) {
+    hll::HyperLogLog<std::string> hll(4);
+    ASSERT_EQ(hll.estimate(), 0);
+
+    std::vector<std::string> test_strings = {
+        "apple", "banana", "cherry", "date", "elderberry",
+        "fig", "grape", "honeydew", "kiwi", "lemon",
+        "apple", "banana", "cherry"
+    };
+
+    for (const auto& str: test_strings) {
+        hll.add(str);
+    }
+
+    uint64_t estimate = hll.estimate();
+
+    EXPECT_GT(estimate, 0);
+}
